77f40ad157275b63ee3a559206c776e352e85c76
yeswasi <54169786+yeswasi@users.noreply.github.com>
2019-10-13 10:30:33 +0900

    Update WebContents.js

--- API/WebContents/v1/WebContents.js
+++ API/WebContents/v1/WebContents.js
@@ -1,7 +1,8 @@
 //プロパティとか
 var WebContents = new Object();
 //定義
-   WebContents.UserAgent = navigator.useragent;
+var ua = window.navigator.userAgent;
+    WebContents.UserAgent = window.navigator.userAgent;
 var bname = "Not Sure";
     WebContents.isChrome = false,
     WebContents.isiOSChrome = false,
@@ -22,7 +23,11 @@ var bname = "Not Sure";
     WebContents.isFacebook = false,
     WebContents.isAndroidApp = false,
     WebContents.isGooglebot = false,
-    WebContents.isSmooz = false;
+    WebContents.isSmooz = false,
+    WebContents.isPSP = false,
+    WebContents.isPS2 = false,
+    WebContents.isPS3 = false,
+    WebContents.isPS4 = false;
 function BrowserName() {
    if (ua.indexOf("Sleipnir") > -1){
       bname = "Sleipnir";
@@ -30,6 +35,18 @@ function BrowserName() {
    } else if (ua.indexOf("Smooz") > -1){
       bname = "Smooz";
       WebContents.isSmooz = true;
+   } else if (ua.indexOf("PSP") > -1) {
+      bname = "PSP";
+      WebContents.isPSP = true;
+   } else if (ua.indexOf("PS2") > -1){
+      bname = "Play Station 2";
+      WebContents.isPS2 = true;
+   } else if (ua.indexOf("PLAYSTATION 3") > -1){
+      bname = "Play Station 3";
+      WebContents.isPS3 = true;
+   } else if (ua.indexOf("PlayStation 4") > -1){
+      bname = "Play Station 4";
+      WebContents.isPS4 = true;
    } else if (ua.indexOf("Googlebot") > -1){
       bname = "Googlebot";
       WebContents.isGooglebot = true;
@@ -90,4 +107,60 @@ function BrowserName() {
    }
    return bname;
 }
-WebContents.BrowserName = bname;
+   WebContents.BrowserName = BrowserName();
+//is...
+   WebContents.isWindows = false;
+   WebContents.isMac = false;
+   WebContents.isiPhone = false;
+   WebContents.UseModel = "Not Sure";
+//どのモデル？
+function UseModel() {
+   if (ua.indexOf("Windows NT") > -1){
+      WebContents.isWindows = true;
+      //Vesion
+      if (ua.indexOf("NT 5.1") > -1){
+         WebContents.UseModel = "Windows XP";
+         } else if (ua.indexOf("NT 5.2") > -1){
+         WebContents.UseModel = "Windows Server 2003";
+         } else if (ua.indexOf("NT 6.0") > -1){
+         WebContents.UseModel = "Windows Vista";
+         } else if (ua.indexOf("NT 6.1") > -1){
+         WebContents.UseModel = "Windows 7";
+         } else if (ua.indexOf("NT 6.2") > -1){
+         WebContents.UseModel = "Windows 8";
+         } else if (ua.indexOf("NT 6.3") > -1){
+         WebContents.UseModel = "Windows 8.1";
+         } else if (ua.indexOf("NT 10.0") > -1){
+         WebContents.UseModel = "Windows 10";
+         }
+   } else if (ua.indexOf("Mac OS X") > -1){
+   WebContents.isMac = true;
+      //Vesion
+      if (ua.indexOf("X 10.5") > -1){
+         WebContents.UseModel = "Macintosh Leopard";
+         } else if (ua.indexOf("X 10.6") > -1){
+         WebContents.UseModel = "Macintosh Snow Leopard";
+         } else if (ua.indexOf("X 10.7") > -1){
+         WebContents.UseModel = "Macintosh Lion";
+         } else if (ua.indexOf("X 10.8") > -1){
+         WebContents.UseModel = "Macintosh Mountain Lion";
+         } else if (ua.indexOf("X 10.9") > -1){
+         WebContents.UseModel = "Macintosh Mavericks";
+         } else if (ua.indexOf("X 10.10") > -1){
+         WebContents.UseModel = "Macintosh Yosemite";
+         } else if (ua.indexOf("X 10.11") > -1){
+         WebContents.UseModel = "Macintosh El Capitan";
+         } else if (ua.indexOf("X 10.12") > -1){
+         WebContents.UseModel = "Macintosh Sierra";
+         } else if (ua.indexOf("X 10.13") > -1){
+         WebContents.UseModel = "Macintosh High Sierra";
+         } else if (ua.indexOf("X 10.14") > -1){
+         WebContents.UseModel = "Macintosh Mojave";
+         } else if (ua.indexOf("X 10.15") > -1){
+         WebContents.UseModel = "Macintosh Catalina";
+         }
+   } else if (ua.indexOf("iPhone") > -1){
+   WebContents.isiPhone = true;
+   WebContents.UseModel = "iPhone";
+   }
+}
